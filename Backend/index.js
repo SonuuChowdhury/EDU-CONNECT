3b4bffb0a1f0303b68731596bdafddd560d7bf81
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-09-02 21:23:11 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -3,19 +3,14 @@ import express from 'express';
 import connectDB from './src/db/index.js';
 import homeRouter from './src/api/homeAPI.js';
 
-
 const app = express();
-
-
-
-
 
 // Connect the Database
 connectDB()
 
-
-
+// routers for home page 
 app.use('/',homeRouter)
+
 
 // Start Server
 const PORT = process.env.PORT || 3000;
