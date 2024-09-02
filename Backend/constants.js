2f9a0f2b649fa43574ca193c333669f5d344de0f
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-09-02 15:58:38 +0530

    Restarting

--- Backend/constants.js
+++ Backend/constants.js
@@ -1,1 +1,6 @@
 export const DB_NAME="login";
+
+
+export function setDB_NAME(New_DB_NAME){
+    DB_NAME=New_DB_NAME
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 // app.js
 import express from 'express';
 import connectDB from './src/db/index.js';
+import { setDB_NAME } from './constants.js';
 const app = express();
 
 // Connect the Database
 connectDB()
+
 
 
 // Start Server
