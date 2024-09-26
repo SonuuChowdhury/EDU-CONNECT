60581a4ee2a393dcfc264dbea4064dfbda715d20
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-09-26 21:18:32 +0530

    console logs improved

--- Backend/index.js
+++ Backend/index.js
@@ -2,28 +2,22 @@
 import express from 'express';
 import cors from 'cors';
 
-
-
-
 // database connection
 import connectDB from './src/db/index.js';
-
 connectDB()
-
-import router from './src/routes/routes.js';
 
 const app = express();
 app.use(cors());
-
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Routes for the server
+import router from './src/routes/routes.js';
 app.use('/',router)
 
 
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`SERVER IS LIVE IN THE PORT 3000 AND PRODUCTION📢!`);
 });
