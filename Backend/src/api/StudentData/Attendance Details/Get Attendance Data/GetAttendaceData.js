7722a375f3beb11995aed2b0db8a561fd2d96409
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-11-17 10:17:15 +0530

    attendance backend minor query fix

--- Backend/src/api/StudentData/Attendance Details/Get Attendance Data/GetAttendaceData.js
+++ Backend/src/api/StudentData/Attendance Details/Get Attendance Data/GetAttendaceData.js
@@ -6,7 +6,7 @@ GetStudentAttendanceDetails.use(express.json());
 
 GetStudentAttendanceDetails.get('/api/student-dashboard/attendance', async (req, res) => {
     const { _id } = req.user;
-    const {roll, getAttendance} = req.body;
+    const {roll, getAttendance} = req.query;
     if(!roll){
         return res.status(404).json({data:"Roll Number is required"})
     }
