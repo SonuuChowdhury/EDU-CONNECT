a209a46f50329b384c08936aef90bf61aed8db7d
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2024-11-18 22:22:52 +0530

    Models added

--- Backend/src/models/students/studentAttendanceDetails.js
+++ Backend/src/models/students/studentAttendanceDetails.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+import  mongoose  from 'mongoose';
 import connectDB from '../../db/ConnectMongoDB.js'
 
 const { studentDetailsConnection } = await connectDB();
 
 const subjectSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Subject name
-  startDate: { type: Date, required: true }, // Date monitoring started
+  startDate: { type: Date, required: true, default: Date.now}, // Date monitoring started
   AbsentDates: { type: [Date], default: [] }, // Array of absent dates
   PresentDates: { type: [Date], default: [] }, // Array of present dates
   LastUpdated: { type: Date, default: Date.now }, // Last update time
-  Monitoring: { type: Boolean, default: false } // Whether monitoring is active
+  Monitoring: { type: Boolean, default: true } // Whether monitoring is active
 });
 
 const attendanceSchema = new mongoose.Schema({
