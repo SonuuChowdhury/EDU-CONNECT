ebdc48c6dc399592da371276c5ef915fcb9c6609
Sonuu.Chowdhury <choudhurysonu2004@gmail.com>
2025-01-25 09:57:25 +0530

    fixed package.json

--- Frontend/src/pages/Test Page/TestPage.jsx
+++ Frontend/src/pages/Test Page/TestPage.jsx
@@ -1,32 +1,12 @@
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // Default styling
 import './TestPage.css';
 
 const MarkedDatesCalendar = () => {
-  const greenDates = ['2024-11-25', '2024-11-28']; // Green marked dates
-  const redDates = ['2024-11-20', '2024-11-30'];   // Red marked dates
-
-  const isGreenDate = (date) => {
-    const formattedDate = date.toISOString().split('T')[0]; // Format to YYYY-MM-DD
-    return greenDates.includes(formattedDate);
-  };
-
-  const isRedDate = (date) => {
-    const formattedDate = date.toISOString().split('T')[0];
-    return redDates.includes(formattedDate);
-  };
-
   return (
-    <Calendar
-      tileClassName={({ date, view }) => {
-        if (view === 'month') {
-          if (isGreenDate(date)) return 'highlight-green';
-          if (isRedDate(date)) return 'highlight-red';
-        }
-        return null;
-      }}
-      onChange={null}
-    />
+    <iframe width="560" height="315" 
+    src="https://www.youtube.com/embed/8af8Dgy43rc?si=p23AURoXrpUkIEjO?rel=0&modestbranding=1&autoplay=0" 
+    title="YouTube video player"  
+    referrerPolicy="strict-origin-when-cross-origin" 
+    allowfullscreen></iframe>
   );
 };
 
